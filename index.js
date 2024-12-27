d323c109084a59f5d9619512701aa79a476e827c
pdt1806 <nmphuc1806@gmail.com>
2024-12-26 23:23:10 -0800

    forbid dot files

--- index.js
+++ index.js
@@ -11,63 +11,78 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const port = 26124;
 
-app.use(express.static("public"));
+app.use((req, res, next) => {
+  if (req.path.startsWith("/.") || req.path.includes("/.")) return res.status(403).send({ error: "naughty naughty" });
+  next();
+});
+
+app.use(
+  express.static("public", {
+    dotfiles: "ignore",
+  })
+);
 
 app.get("/*", (req, res) => {
-  const directoryPath = path.join(__dirname, "public", req.params[0] || "");
-  fs.stat(directoryPath, (err, stats) => {
-    if (err) return res.status(404).send({ error: "Not found" });
+  try {
+    const directoryPath = path.join(__dirname, "public", req.params[0] || "");
+    fs.stat(directoryPath, (err, stats) => {
+      if (err) return res.status(404).send({ error: "Not found" });
 
-    if (stats.isDirectory()) {
-      fs.readdir(directoryPath, async (err, files) => {
-        if (err) return res.status(500).send("Unable to scan directory");
+      if (stats.isDirectory()) {
+        fs.readdir(directoryPath, async (err, files) => {
+          if (err) return res.status(500).send({ error: "Internal server error" });
 
-        const parentPath = path.join(req.path, "..");
+          const parentPath = path.join(req.path, "..");
 
-        const processFile = async (file) => {
-          const filePath = path.join(req.path, file);
-          const absoluteFilePath = path.join(directoryPath, file);
+          const processFile = async (file) => {
+            const filePath = path.join(req.path, file);
+            const absoluteFilePath = path.join(directoryPath, file);
 
-          const fileStats = fs.statSync(absoluteFilePath);
+            const fileStats = fs.statSync(absoluteFilePath);
 
-          const fileType = await fileTypeFromFile(absoluteFilePath).catch(() => undefined);
+            const fileType = await fileTypeFromFile(absoluteFilePath).catch(() => undefined);
 
-          const mimeCategory = fileType ? fileType.mime : fileStats.isDirectory() ? "directory" : "unknown";
+            const mimeCategory = fileType ? fileType.mime : fileStats.isDirectory() ? "directory" : "unknown";
 
-          const mimeTypesKeys = Object.keys(mimeTypes);
+            const mimeTypesKeys = Object.keys(mimeTypes);
 
-          const mimeTypeCategory =
-            !fileType && !fileStats.isDirectory()
-              ? "unknown"
-              : mimeTypesKeys.find((key) => {
-                  return mimeCategory.includes(key) || (fileType.ext && fileType.ext === key);
-                }) ||
-                mimeTypesKeys.find((key) => mimeCategory.includes(key)) ||
-                mimeTypesKeys.find((key) => mimeCategory.startsWith(key)) ||
-                "unknown";
+            const mimeTypeCategory =
+              !fileType && !fileStats.isDirectory()
+                ? "unknown"
+                : mimeTypesKeys.find((key) => {
+                    return mimeCategory.includes(key) || (fileType.ext && fileType.ext === key);
+                  }) ||
+                  mimeTypesKeys.find((key) => mimeCategory.includes(key)) ||
+                  mimeTypesKeys.find((key) => mimeCategory.startsWith(key)) ||
+                  "unknown";
 
-          const emoji = mimeTypes[mimeTypeCategory];
+            const emoji = mimeTypes[mimeTypeCategory];
 
-          return `<a href="${filePath}">${emoji} ${file}</a>`;
-        };
+            return `<a href="${filePath}">${emoji} ${file}</a>`;
+          };
 
-        const sortedFiles = files.sort((a, b) => {
-          const aHasDot = a.includes(".");
-          const bHasDot = b.includes(".");
-          if (aHasDot && !bHasDot) return 1;
-          if (!aHasDot && bHasDot) return -1;
-          return a.localeCompare(b);
+          const sortedFiles = files
+            .filter((file) => !file.startsWith("."))
+            .sort((a, b) => {
+              const aHasDot = a.includes(".");
+              const bHasDot = b.includes(".");
+              if (aHasDot && !bHasDot) return 1;
+              if (!aHasDot && bHasDot) return -1;
+              return a.localeCompare(b);
+            });
+
+          const fileLinks = await Promise.all(sortedFiles.map((file) => processFile(file))).then((results) =>
+            results.reduce((acc, curr) => acc + curr, "")
+          );
+
+          const backButton = req.path !== "/" ? `<a href="${parentPath}">🔙 Back</a><br />` : "";
+          res.send(`${style}${backButton}${fileLinks}`);
         });
-
-        const fileLinks = await Promise.all(sortedFiles.map((file) => processFile(file))).then((results) =>
-          results.reduce((acc, curr) => acc + curr, "")
-        );
-
-        const backButton = req.path !== "/" ? `<a href="${parentPath}">🔙 Back</a><br />` : "";
-        res.send(`${style}${backButton}${fileLinks}`);
-      });
-    } else res.sendFile(directoryPath);
-  });
+      } else res.sendFile(directoryPath);
+    });
+  } catch (error) {
+    res.status(500).send({ error: "Internal server error" });
+  }
 });
 
 app.listen(port, console.log(`File server running at http://localhost:${port}`));
